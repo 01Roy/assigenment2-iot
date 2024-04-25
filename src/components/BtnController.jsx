d7604764f9163ff70f895e5dcda797da061c6ef2
roy01 <subhamroy1180@gmail.com>
2024-04-25 21:25:20 +0530

    d2

--- src/components/BtnController.jsx
+++ src/components/BtnController.jsx
@@ -9,7 +9,12 @@ function BtnController() {
   return (
     <div className="">
       <div className="w-[30rem] h-[10rem] bg-[#D9D9D9] relative rounded-2xl mt-[8rem] ml-[-2rem]">
-        <div className="absolute w-[29.7rem] h-[9.8rem]  backimage2 bg-cover top-[2px] left-[1px] rounded-xl ">
+        <div className="absolute w-[29.7rem] h-[9.8rem] bg-cover top-[2px] left-[1px] rounded-xl ">
+          <img
+            src="./public/Rectangle29.png"
+            alt=""
+            className="absolute w-[29.7rem] h-[9.8rem] bg-cover top-[1px] left-[1px] rounded-xl"
+          />
           <div className="w-full flex justify-center align-middle gap-[6rem] mt-[1.2rem] ">
             {/* first */}
             <div onClick={() => swiper.slidePrev()}>
--- src/components/SliderGrop1/SliderGrop1.jsx
+++ src/components/SliderGrop1/SliderGrop1.jsx
@@ -4,7 +4,12 @@ import SliderComponent from "../SliderComponent";
 function SliderGrop1() {
   return (
     <div className="w-[30rem] h-[30rem] bg-[#D9D9D9] relative m-[10rem] rounded-2xl">
-      <div className="absolute w-[29.8rem] h-[29.8rem] backimage bg-cover top-[.5px] left-[1px] rounded-xl ">
+      <img
+        src="./public/Rectangle.png"
+        alt=""
+        className="absolute w-[29.8rem] h-[29.8rem] bg-cover top-[.5px] left-[1px] rounded-xl"
+      />
+      <div className=" ">
         <SliderComponent className="absolute " />
       </div>
     </div>
