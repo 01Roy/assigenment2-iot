202d124ce7adaa9560240bb8385eb1c9bdc265f1
roy01 <subhamroy1180@gmail.com>
2024-04-25 21:37:46 +0530

    d3

--- src/components/SliderGrop1/SliderGrop1.jsx
+++ src/components/SliderGrop1/SliderGrop1.jsx
@@ -5,7 +5,7 @@ function SliderGrop1() {
   return (
     <div className="w-[30rem] h-[30rem] bg-[#D9D9D9] relative m-[10rem] rounded-2xl">
       <img
-        src="./public/Rectangle.png"
+        src="https://res.cloudinary.com/dcnjbnpnl/image/upload/fl_preserve_transparency/v1714060848/Rectangle_gj3x78.jpg?_s=public-apps"
         alt=""
         className="absolute w-[29.8rem] h-[29.8rem] bg-cover top-[.5px] left-[1px] rounded-xl"
       />
